938de290bc51fb1b87848d36dd9c316bfba73581
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2017-01-21 00:52:52 -0500

    ask question for a selection

--- sous_chef.py
+++ sous_chef.py
@@ -6,18 +6,26 @@ ask = Ask(app, '/')
 
 @ask.launch
 def launch():
-    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    speech_text = 'Welcome to Sous Chef, you can ask me for recipes.'
+    return question(speech_text).reprompt(speech_text)
 
-@ask.intent('HelloWorldIntent')
-def hello_world():
-    speech_text = 'Hello world'
-    return statement(speech_text).simple_card('HelloWorld', speech_text)
+@ask.intent('RecipeIntent')
+def recipe(Dish):
+    text = render_template('recipe', Dish=Dish)
+    text += " Which would you like?"
+    reprompt_text = "I didn't catch that. What would you like to make?"
+    return question(text).reprompt(reprompt_text)
+
+@ask.intent('PickIntent')
+def ingredient(Selection):
+    text = render_template('ingredients', Selection=Selection)
+    return statement(text).simple_card('Ingredients for ' + Selection, text)
+
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
-    speech_text = 'You can say hello to me!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    speech_text = 'You can ask me for recipes!'
+    return question(speech_text).reprompt(speech_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
