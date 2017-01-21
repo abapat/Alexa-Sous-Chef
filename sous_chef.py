00c19d57e80452086e833f24eac79eb40ef58772
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2017-01-21 02:08:06 -0500

    selection from a list

--- sous_chef.py
+++ sous_chef.py
@@ -12,19 +12,26 @@ def launch():
 @ask.intent('RecipeIntent')
 def recipe(Dish):
     text = render_template('recipe', Dish=Dish)
-    text += " Which would you like?"
+    text += " Dish 1: beef stew. Dish 2: beef stroganoff. Please say the dish number you'd like to make, or say more for more options."
     reprompt_text = "I didn't catch that. What would you like to make?"
+    session.attributes['dish'] = Dish
     return question(text).reprompt(reprompt_text)
 
-@ask.intent('PickIntent')
+@ask.intent('PickIntent', convert={'Selection': int})
 def ingredient(Selection):
-    text = render_template('ingredients', Selection=Selection)
-    return statement(text).simple_card('Ingredients for ' + Selection, text)
+    text = render_template('ingredients', Selection=str(Selection))
+    if Selection == 1:
+        text += " one one one"
+
+    if Selection == 2:
+        text += " two two two"
+
+    return statement(text).simple_card('Ingredients for dish ' + str(Selection), text)
 
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
-    speech_text = 'You can ask me for recipes!'
+    speech_text = 'You can ask me for recipes! Like, recipes for beef stew. Or, tell me how to make Philly Cheesesteaks.'
     return question(speech_text).reprompt(speech_text)
 
 if __name__ == '__main__':
